07b9cc5b309f4a5afcb2888430cde901e1dc0027
Zsolt Sztranyák <zsolt.sztranyak80@gmail.com>
2023-11-23 08:20:06 +0100

    AOC 2022-25 - using class

--- 2022/day_25.py
+++ 2022/day_25.py
@@ -1,36 +1,46 @@
 from GENERICS.aoc2 import yield_input_data, aoc_solve_puzzle
 
 
-def snafu_to_dec(p_snafu: str) -> int:
-    return_int = 0
-    for d in p_snafu:
-        return_int = return_int * 5 + '=-012'.index(d) - 2
-    return return_int
+class CSnafu:
+    def __init__(self, p_value: str):
+        self.value = p_value
 
+    @property
+    def value(self):
+        p_dec = self.value_int
+        new_add = 0
+        return_str = ''
+        while p_dec != 0:
+            new_digit_div = p_dec % 5
+            p_dec = p_dec // 5
+            return_str = '012=-0'[new_digit_div + new_add] + return_str
+            if new_digit_div + new_add in [0, 1, 2]:
+                new_add = 0
+            else:
+                new_add = 1
+        if new_add in [1, 2]:
+            return_str = str(new_add) + return_str
+        return return_str
 
-def dec_to_snafu(p_dec: int) -> str:
-    new_add = 0
-    return_str = ''
-    while p_dec != 0:
-        new_digit_div = p_dec % 5
-        p_dec = p_dec // 5
-        return_str = '012=-0'[new_digit_div + new_add] + return_str
-        if new_digit_div + new_add in [0, 1, 2]:
-            new_add = 0
-        else:
-            new_add = 1
-    if new_add in [1, 2]:
-        return_str = str(new_add) + return_str
-    return return_str
+    @value.setter
+    def value(self, p_value):
+        self.value_int = 0
+        for d in p_value:
+            self.value_int = self.value_int * 5 + '=-012'.index(d) - 2
+
+    def __add__(self, other):
+        rs = CSnafu('0')
+        rs.value_int = self.value_int + other.value_int
+        return rs
 
 
 def solve_puzzle(p_input_file_path: str) -> (int | str, int | str | None):
 
     answer2 = None
-    act_num = 0
+    act_num = CSnafu('0')
     for inp_row in yield_input_data(p_input_file_path, p_whole_row=True, p_convert_to_num=False):
-        act_num += snafu_to_dec(inp_row)
-    answer1 = dec_to_snafu(act_num)
+        act_num += CSnafu(inp_row)
+    answer1 = act_num.value
 
     return answer1, answer2
 
