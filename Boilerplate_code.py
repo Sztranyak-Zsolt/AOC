e63c7e20e4823c0686f5db574d92850fee09f647
Zsolt Sztranyák <zsolt.sztranyak80@gmail.com>
2023-12-29 12:38:45 +0100

    Adjust boilerplate code.

--- Boilerplate_code.py
+++ Boilerplate_code.py
@@ -5,20 +5,24 @@ from GENERICS.aoc2 import yield_input_data, aoc_solve_puzzle
 # from itertools import product, permutations, combinations, combinations_with_replacement
 # from functools import cache, cached_property
 # from math import prod, lcm, gcd
+# from heapq import heapify, heappop, heappush
 
 
 def solve_puzzle(p_input_file_path: str) -> (int | str, int | str | None):
-    answer1 = answer2 = None
+    answer1 = answer2 = 0
     # input_iterator = iter(yield_input_data(p_input_file_path, p_whole_row=True))
     # input_single_row = next(input_iterator)
-    for inp_row in yield_input_data(p_input_file_path):
-        pass
+    for inp_row in yield_input_data(p_input_file_path,
+                                    p_whole_row=False,
+                                    p_chars_to_space=''
+                                    ):
+        print(inp_row)
 
     return answer1, answer2
 
 
 def main():
-    aoc_solve_puzzle(2023, 0, solve_puzzle)
+    aoc_solve_puzzle(2024, 0, solve_puzzle)
 
 
 if __name__ == '__main__':
